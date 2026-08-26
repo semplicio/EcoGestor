92158b8f0dfc49aa811bdad3f11b043eac2f9265
semplicio <60362115+semplicio@users.noreply.github.com>
2026-08-26 20:13:46 -0300

    build: remove plugin Kotlin Android incompatível com AGP 9

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,5 @@
 plugins {
     id("com.android.application")
-    id("org.jetbrains.kotlin.android")
     id("org.jetbrains.kotlin.plugin.compose")
 }
 
@@ -45,10 +44,6 @@ android {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
         }
     }
-}
-
-kotlin {
-    jvmToolchain(17)
 }
 
 dependencies {
